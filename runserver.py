2bf368fa7fc53cc8d691ce941bb3cf39d774cc8a
osalivan <bracanier@gmail.com>
2016-07-26 01:35:36 +0300

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     app = Pogom(__name__)
 
     if args.cors:
-        CORS(app, resources={r"/api/*": {"origins": "*"}})
+        CORS(app, resources={r"/": {"origins": "http://www.po-go.online/"}})
 
     config['ROOT_PATH'] = app.root_path
     config['GMAPS_KEY'] = args.gmaps_key
